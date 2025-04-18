3c2fe223df72291abc380f9ddae713121e2efafa
Alex Richards <alex.h.richards@outlook.com>
2025-04-18 13:45:24 -0500

    Created the projects page with test content. Added navigation functionality to the application.

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, //Default
-  { path: 'home', component: HomeComponent }, //Home
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent },
+
 ];
 
 @NgModule({
