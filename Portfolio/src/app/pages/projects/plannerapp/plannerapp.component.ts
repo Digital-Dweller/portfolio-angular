ffaf116f361f50335c78cb582f4db73672d296d8
Alex Richards <alex.h.richards@outlook.com>
2025-04-28 20:57:34 -0500

    Added content to the Degree Planner project page. Page formatting adjustments are still required.

--- Portfolio/src/app/pages/projects/plannerapp/plannerapp.component.ts
+++ Portfolio/src/app/pages/projects/plannerapp/plannerapp.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './plannerapp.component.css'
 })
 export class PlannerappComponent {
-
+  componentImages: string[] = [
+    'resources/degree_planner/planner_screenshot1.png',
+    'resources/degree_planner/planner_screenshot2.png',
+    'resources/degree_planner/planner_screenshot3.png',
+    'resources/degree_planner/planner_screenshot4.png',
+    'resources/degree_planner/planner_screenshot5.png',
+    'resources/degree_planner/planner_screenshot6.png',
+    'resources/degree_planner/planner_screenshot7.png',
+    'resources/degree_planner/planner_screenshot8.png',
+    'resources/degree_planner/planner_screenshot9.png',
+    'resources/degree_planner/planner_screenshot10.png',
+    'resources/degree_planner/planner_screenshot11.png',
+    'resources/degree_planner/planner_screenshot12.png',
+    'resources/degree_planner/planner_screenshot13.png',
+    'resources/degree_planner/planner_screenshot14.png'
+  ];
 }
