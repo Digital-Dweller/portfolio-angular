1d820958f322ea7b00f8ceb9b5dd357224778266
Alex <ari1268@wgu.edu>
2025-05-20 15:58:35 -0500

    Adjusted the image viewer component so the navigation buttons won't change when the image width does. Added page content for the about page and changed the placeholder images to the image viewer component.

--- Portfolio/src/app/pages/about/about.component.ts
+++ Portfolio/src/app/pages/about/about.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  aboutImages1: string[] = [
+    'resources/about_images/aboutPic1.png',
+    'resources/invManager/invMan_screenshot2.jpg',
+    'resources/invManager/invMan_screenshot3.jpg',
+    'resources/invManager/invMan_screenshot4.jpg',
+    'resources/invManager/invMan_screenshot5.jpg',
+    'resources/invManager/invMan_screenshot6.jpg'
+  ];
 }
