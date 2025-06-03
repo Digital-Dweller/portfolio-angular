c3535169204e0a1a0707a1ce239155528b536cf7
Alex <ari1268@wgu.edu>
2025-06-03 14:13:13 -0500

    Added a hamburger-style navigation menu for mobile devices.

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  menuOpen = false;
+  menuOpen_projects = false;
+
+  constructor(private router: Router) { }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  closeMenu() {
+    this.menuOpen = false;
+  }
+  toggleProjects(event: Event) {
+    if (window.innerWidth < 768) {
+      event.preventDefault();
+      this.menuOpen_projects = !this.menuOpen_projects;
+    }
+  }
+  
+  closeProjects() {
+    this.menuOpen_projects = false;
+  }
+
+  isProjectSubpageActive(): boolean {
+    return this.router.url.startsWith('/projects/');
+  }
 }
